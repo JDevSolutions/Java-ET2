4bc3c8d4f795f9cdf77c759dac0f54bd5a30e444
SpinFoam <64492048+Diffeomorph@users.noreply.github.com>
2021-05-07 21:41:48 +0100

    initial

---
+++ java-et2/src/main/java/com/self/javaet2/JavaEt2Application.java
@@ -0,0 +1,13 @@
+package com.self.javaet2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEt2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEt2Application.class, args);
+	}
+
+}
---
+++ java-et2/src/test/java/com/self/javaet2/JavaEt2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self.javaet2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEt2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
