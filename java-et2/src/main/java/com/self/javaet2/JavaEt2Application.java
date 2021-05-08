682d9386280e93b52b12ada31b17d9382012e450
SpinFoam <64492048+Diffeomorph@users.noreply.github.com>
2021-05-08 18:00:37 +0100

    fixednullbook

--- java-et2/src/main/java/com/self/javaet2/JavaEt2Application.java
+++ java-et2/src/main/java/com/self/javaet2/JavaEt2Application.java
@@ -1,17 +1,17 @@
 package com.self.javaet2;
 
-import com.self.javaet2.controller.WebController;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
 public class JavaEt2Application {
 
-	public static OrderBook book;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaEt2Application.class, args);
 	}
-
+		public static OrderBook book = new OrderBook();
 }
--- java-et2/src/main/java/com/self/javaet2/controller/WebController.java
+++ java-et2/src/main/java/com/self/javaet2/controller/WebController.java
@@ -1,39 +1,41 @@
 package com.self.javaet2.controller;
 
-import com.self.javaet2.JavaEt2Application;
 import com.self.javaet2.Order;
 import org.springframework.web.bind.annotation.*;
+
+import static com.self.javaet2.JavaEt2Application.book;
 
 @RestController
 public class WebController {
 
     @GetMapping("/print_order_book")
     void printOrderBook(){
-        JavaEt2Application.book.printOrderBook();
+        book.printOrderBook();
         return;
     }
 
     @GetMapping("/find_min_buy")
     Double findMinBuy(){
-        Double maxBuy = JavaEt2Application.book.findMaxBuy();
+        Double maxBuy = book.findMaxBuy();
+
         return maxBuy;
     }
 
-    @GetMapping("/find_max_buy")
+    @GetMapping("/find_max_sell")
     Double findMaxSell(){
-        Double minSell = JavaEt2Application.book.findMinSell();
+        Double minSell = book.findMinSell();
         return minSell;
     }
 
     @PostMapping("/submit_buy_trade")
     Order submitBuyTrade(@RequestParam double size, @RequestParam double limit){
-        Order order = JavaEt2Application.book.sendBuyLimitOrder(size, limit);
+        Order order = book.sendBuyLimitOrder(size, limit);
         return order;
     }
 
     @PostMapping("/submit_sell_trade")
     Order submitSellTrade(@RequestParam double size, @RequestParam double limit){
-        Order order = JavaEt2Application.book.sendSellLimitOrder(size, limit);
+        Order order = book.sendSellLimitOrder(size, limit);
         return order;
     }
 
